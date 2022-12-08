f1e784deb6c9899a900aab095bed2d388dccb5fb
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-08 10:10:24 +0300

    Add files via upload

---
+++ ConsoleApp_5/ConsoleApp_5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_5
+{
+    internal class Program
+    {
+        static double f(double x)
+        {
+            return Math.Sqrt(x * x + 1) - Math.Sqrt(x * x + 5);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("a=");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b=");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("h=");
+                double h = double.Parse(Console.ReadLine());
+
+                if (a > b)
+                {
+                    Console.Write("Параметр a не может быть больше b");
+                    return;
+                }
+
+                if (h <= 0)
+                {
+                    Console.Write("Параметр h не может быть меньше или равен нулю");
+                    return;
+                }
+
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        Console.WriteLine("y({0})={1:f4}", i, f(i));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("y({0})=error", i);
+                    }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода данных");
+            }
+            catch
+            {
+                Console.WriteLine("Неизвестная ошибка");
+            }
+        }
+    }
+}
---
+++ ConsoleApp_5_form/ConsoleApp_5_form/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_5_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double f(double x)
+        {
+            return Math.Sqrt(x * x + 1) - Math.Sqrt(x * x + 5);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double h = double.Parse(textBox3.Text);
+
+                if (a > b)
+                {
+                    textBox4.Text += "Параметр a не может быть больше b";
+                    return;
+                }
+
+                if (h <= 0)
+                {
+                    textBox4.Text += "Параметр h не может быть меньше или равен нулю";
+                    return;
+                }
+
+                for (double i = a; a <= b; a += h)
+                    try
+                    {
+                        textBox4.Text += ($"y({a})={f(a)}") + Environment.NewLine;
+                    }
+                    catch
+                    {
+                        textBox4.Text += ($"y({a})=error") + Environment.NewLine;
+                    }
+            }
+            catch (FormatException)
+            {
+                textBox4.Text += ("Неверный формат ввода данных");
+            }
+            catch
+            {
+                textBox4.Text += ("Неизвестная ошибка");
+            }
+        }
+    }
+}
